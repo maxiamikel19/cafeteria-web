c9c499913d92097d7c20fb4c34e18bcfd4999f6c
maxiamikel19 <maxloversist19@gmail.com>
2024-07-29 00:00:05 -0300

    Implementar GUI de cadastro

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,10 +2,15 @@ import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
   return (
-    <div>
-      <h1 className='text-4xl text-red-600'>Auth Layout</h1>
-
-      <Outlet></Outlet>
+    <main className="max-w-4xl m-auto mt-5 md:mt-5 flex flex-col md:flex-row items-center">
+      <img 
+        src="../img/logo-sf1.png" 
+        alt="Logo cafeteria" 
+        className="max-w-xs "
+    />
+    <div className="w-full p-10 bg-white shadow-lg rounded-lg">
+        <Outlet></Outlet>
     </div>
+    </main>
   )
 }
