59b819d1e0a11ceb95a1f2b6ff6f15f47f501d5c
maxiamikel19 <maxloversist19@gmail.com>
2024-07-29 09:20:07 -0300

    Renomear arquivos

---
+++ src/views/Registro.jsx
@@ -0,0 +1,96 @@
+import { Link } from "react-router-dom";
+
+export default function Registro() {
+  return (
+    <>
+      <h1 className="text-2xl text-center">Criar sua conta</h1>
+      <p className="text-sm text-center">Informe  os dados silicitados no formulario para criar sua conta</p>
+
+      <div className="bg-white shadow-md rounded-md mt10 px-5 py-10">
+        <form action="" autoComplete="OFF">
+            <div className="mb-4">
+                <label 
+                    htmlFor="name"
+                    className="text-slate-800"
+                >Nome</label>
+                <input 
+                    type="text" 
+                    className="mt-2 w-full p-3 bg-gray-50 "
+                    id="name"
+                    name="name"
+                    placeholder="Ex: Amikel Maxi"
+                />
+            </div>
+
+            <div className="mb-4">
+                <label 
+                    htmlFor="telefone"
+                    className="text-slate-800"
+                >Telefone</label>
+                <input 
+                    type="text" 
+                    className="mt-2 w-full p-3 bg-gray-50 border rounded"
+                    id="telefone"
+                    name="telefone"
+                    placeholder="Ex: 44 9 99999999"
+                />
+            </div>
+
+            <div className="mb-4">
+                <label 
+                    htmlFor="email"
+                    className="text-slate-800"
+                >Email</label>
+                <input 
+                    type="email" 
+                    className="mt-2 w-full p-3 bg-gray-50 border rounded"
+                    id="email"
+                    name="email"
+                    placeholder="Ex: amikelmaxi@email.com"
+                />
+            </div>
+
+            <div className="mb-4">
+                <label 
+                    htmlFor="password"
+                    className="text-slate-800"
+                >Senha</label>
+                <input 
+                    type="password" 
+                    className="mt-2 w-full p-3 bg-gray-50 border rounded"
+                    id="password"
+                    name="password"
+                    placeholder="Ex: ******"
+                />
+            </div>
+
+            <div className="mb-4">
+                <label 
+                    htmlFor="password_confirmation"
+                    className="text-slate-800"
+                >Confirmar a senha</label>
+                <input 
+                    type="password" 
+                    className="mt-2 w-full p-3 bg-gray-50 border rounded"
+                    id="password_confirmation"
+                    name="password_confirmation"
+                    placeholder="Ex: ******"
+                />
+            </div>
+
+            <input 
+                type="submit" 
+                value="Criar sua conta"
+                className="mt-8 bg-slate-800 cursor-pointer hover:bg-slate-600 uppercase w-full p-4 text-slate-400 rounded-lg"
+            />
+        </form>
+      </div>
+
+      <nav className="mt-5 text-slate-600">
+        <Link to="/auth/login">
+            J&aacute; tem conta? Logar
+        </Link>
+      </nav>
+    </>
+  )
+}
