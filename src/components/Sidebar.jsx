dbfe4af8676fa53566417646bf5dbe668b27cf82
maxiamikel19 <maxloversist19@gmail.com>
2024-07-29 12:24:43 -0300

    Adicionar imagen para categorias e implementar o Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,34 @@
+import { categorias } from "../data/categorias";
+import Categoria from "./Categoria";
+
 export default function Sidebar() {
   return (
-    <aside className="md:w-72">
-      <h1>Sidebar</h1>
+    <aside className="md:w-72 border">
+      <div className="p-4">
+        <img 
+            src="../img/logo-oficials.png" 
+            alt="Alide Logo" 
+            className="w-40 m-auto"
+        />
+      </div>
+      <div className="mt-4">
+        <h1 className="text-center text-xl tracking-tight p-4 bg-slate-600 text-blue-300 font-semibold">Categorias</h1>
+        {categorias.map(categoria => (
+            <Categoria 
+              key={categoria.id}
+              categoria = {categoria}
+            />
+        ))}
+      </div>
+
+      <div className="my-5 py-5">
+        <button
+          className="w-full bg-slate-600 text-blue-300 font-semibold p-3 truncate"
+          type="button"
+        >
+          Cancelar pedido
+        </button>
+      </div>
     </aside>
   )
 }
---
+++ src/data/categorias.js
@@ -0,0 +1,55 @@
+const categorias = [
+    {
+        id: 1,
+        nome: "Café",
+        icono: "cafe.png"
+    },
+    {
+        id: 2,
+        nome: "almoço",
+        icono: "almoco.png"
+    },
+    {
+        id: 3,
+        nome: "bolo",
+        icono: "bolo.png"
+    },
+    {
+        id: 4,
+        nome: "cha",
+        icono: "cha.png"
+    },
+    {
+        id: 5,
+        nome: "churrasco",
+        icono: "churrasco.png"
+    },
+    {
+        id: 6,
+        nome: "coxinha",
+        icono: "coxinha.png"
+    },
+    {
+        id: 7,
+        nome: "hamburger",
+        icono: "hamburger.png"
+    },
+    {
+        id: 8,
+        nome: "pao",
+        icono: "pao.png"
+    },
+    {
+        id: 9,
+        nome: "pescado",
+        icono: "pescado.png"
+    },
+    {
+        id: 10,
+        nome: "pizza",
+        icono: "pizza.png"
+    }
+ 
+]
+
+export {categorias}
